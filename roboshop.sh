018203bfd01057e81e083fade0328d6dd4b8808b
Vineethk08 <milatariv@mail.sacredheart.edu>
2026-01-31 08:46:03 -0500

    learning

--- roboshop.sh
+++ roboshop.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-set -euo pipefail
+
+
 SG_ID="sg-0397ae3d261c3d2e4" # Replace with your actual Security Group ID
 AMI_ID="ami-0220d79f3f480ecf5" # Replace with your desired AMI ID
+Zone_ID="0760197PITCJBO2DZXK" # Replace with your actual Hosted Zone ID
+Domain_Name="devopspractice08.online" # Replace with your actual Domain Name
 
 for instance in "$@"; do
     instance_ID=$(aws ec2 run-instances \
@@ -16,15 +19,48 @@ for instance in "$@"; do
         ip=$(aws ec2 describe-instances \
             --instance-ids "$instance_ID" \
             --query 'Reservations[0].Instances[0].PublicIpAddress' \
-            --output text)
+            --output text
+            )
+
+        RECORD_NAME="$Domain_Name" # Replace with your desired record name
+
     else
         ip=$(aws ec2 describe-instances \
             --instance-ids "$instance_ID" \
             --query 'Reservations[0].Instances[0].PrivateIpAddress' \
             --output text)
+
+        RECORD_NAME="$instance.$Domain_Name" # Replace with your desired record name
     fi
 
     echo "The IP address: $ip"
+
+    aws route53 change-resource-record-sets \
+        --hosted-zone-id $Zone_ID \
+        --change-batch '
+    {         {
+  "Comment": "Updating record",
+  "Changes": [
+    {
+      "Action": "UPSERT",
+      "ResourceRecordSet": {
+        "Name": "'$RECORD_NAME'",
+        "Type": "A",
+        "TTL": 1,
+        "ResourceRecords": [
+          {
+            "Value": "'$ip'"
+          }
+        ]
+      }
+    }
+  ]
+}
+'
+
+
+    
+    echo "Record created for $instance"
 
 
 
