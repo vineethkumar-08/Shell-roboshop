3e5609fc488a370595677eaceceb3998406f9fee
Vineethk08 <milatariv@mail.sacredheart.edu>
2026-01-31 08:17:53 -0500

    learning

--- roboshop.sh
+++ roboshop.sh
@@ -2,16 +2,17 @@
 SG_ID=" sg-0397ae3d261c3d2e4" # Replace with your actual Security Group ID
 AMI_ID="ami-0220d79f3f480ecf5" # Replace with your desired AMI ID
 
-fot instance in $@
+for instance in $@
+
 do
-$(aws ec2 run-instances \
+    instance_ID=$(aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type t3 .micro \
     --security-group-ids $SG_ID \ 
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \ 
     --output text)
-    
+
     if [ $instance == "frontend" ]; then
         ip=$ (
            aws ec2 describe-instances \ 
