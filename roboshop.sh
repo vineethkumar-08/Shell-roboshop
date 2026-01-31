39c87b6db4074603f9f0a60e0a39847b03b3c0ab
Vineethk08 <milatariv@mail.sacredheart.edu>
2026-01-31 08:24:58 -0500

    learning

--- roboshop.sh
+++ roboshop.sh
@@ -1,35 +1,30 @@
 #!/bin/bash
-SG_ID=" sg-0397ae3d261c3d2e4" # Replace with your actual Security Group ID
+set -euo pipefail
+SG_ID="sg-0397ae3d261c3d2e4" # Replace with your actual Security Group ID
 AMI_ID="ami-0220d79f3f480ecf5" # Replace with your desired AMI ID
 
-for instance in $@
-
-do
+for instance in "$@"; do
     instance_ID=$(aws ec2 run-instances \
-    --image-id $AMI_ID \
-    --instance-type t3 .micro \
-    --security-group-ids $SG_ID \ 
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-    --query 'Instances[0].InstanceId' \ 
-    --output text)
+        --image-id "$AMI_ID" \
+        --instance-type t3.micro \
+        --security-group-ids "$SG_ID" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+        --query 'Instances[0].InstanceId' \
+        --output text)
 
-    if [ $instance == "frontend" ]; then
-        ip=$(
-           aws ec2 describe-instances \ 
-           --instance-ids $instance_ID\ 
-           --query 'Reservations[0].Instances[0].PublicIpAddress' \
-        )
-        else
-        ip=$ (
-           aws ec2 describe-instances \ 
-           --instance-ids $instance_ID\ 
-           --query 'Reservations[0].Instances[0].PrivateIpAddress' \
-        )
-        echo "The IP address  $ip"
-
-
-
+    if [ "$instance" = "frontend" ]; then
+        ip=$(aws ec2 describe-instances \
+            --instance-ids "$instance_ID" \
+            --query 'Reservations[0].Instances[0].PublicIpAddress' \
+            --output text)
+    else
+        ip=$(aws ec2 describe-instances \
+            --instance-ids "$instance_ID" \
+            --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+            --output text)
     fi
+
+    echo "The IP address: $ip"
 
 
 
