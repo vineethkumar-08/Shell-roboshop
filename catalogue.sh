0817e21c1a07e92c67fb60996d12c950f626980f
Vineethk08 <milatariv@mail.sacredheart.edu>
2026-02-02 12:07:02 -0500

    learning

--- catalogue.sh
+++ catalogue.sh
@@ -3,14 +3,13 @@
 LOGS_FOLDER="/var/logs/shell-roboshop"  #defining logs folder path
 LOGS_FILE="$LOGS_FOLDER/$0.log"   #defining log file path
 
-R="\e[31m"   
-G="\e[32m"   
-Y="\e[33m"    
-N="\e[0m"    
+R="\033[31m"
+G="\033[32m"
+Y="\033[33m"
+N="\033[0m"    
 
 if [ $USERID -ne 0 ] ; then 
-    echo "$R Please run this script with root acess $N" | tee -a $LOGS_FILE     #run as root user 
-    exit 1 # exit scode
+echo -e "$R Please run this script with root acess $N" | tee -a $LOGS_FILE    exit 1 # exit scode
 fi
     mkdir -p $LOGS_FOLDER  #creating logs folder if not exists
 
@@ -18,12 +17,11 @@ fi
 VALIDATE(){
      
     if [ $1 -ne 0 ] ; then
-        echo " $2 $Rinstallation failed...$N" | tee -a $LOGS_FILE 
-        exit 1
+  echo -e " $2 ${R}installation failed...${N}" | tee -a $LOGS_FILE        exit 1
     else
 
-        echo " $2 $Ginstallation successful....$N"  | tee -a $LOGS_FILE
-fi
+      echo -e " $2 ${G}installation successful....${N}" | tee -a $LOGS_FILE
+      fi
 }
 
 dnf module disable nodejs -y &>> $LOGS_FILE
@@ -35,17 +33,17 @@ VALIDATE $? "Enabling Nodejs 20 module"
 dnf install nodejs -y &>> $LOGS_FILE
 VALIDATE $? "Nodejs installation"
 
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>> $LOGS_FILE
 VALIDATE $? "Adding roboshop user"
 
 mkdir /app &>> $LOGS_FILE
 VALIDATE $? "Creating application directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>> $LOGS_FILE
 cd /app 
 unzip /tmp/catalogue.zip &>> $LOGS_FILE
 VALIDATE $? "Extracting application content"
 
 cd /app 
 npm install &>> $LOGS_FILE
-VALIDATE $? "Installing application dependencies"
+VALIDATE $? "Installing application dependencies" 
