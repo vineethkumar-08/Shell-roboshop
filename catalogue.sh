2404ef10f906d0148a4a82c01f9450230f9a2088
Vineethk08 <milatariv@mail.sacredheart.edu>
2026-02-02 14:06:32 -0500

    learning

--- catalogue.sh
+++ catalogue.sh
@@ -1,96 +1,86 @@
+
 #!/bin/bash
- USERID=$(id -u)
-LOGS_FOLDER="/var/logs/shell-roboshop"  #defining logs folder path
-LOGS_FILE="$LOGS_FOLDER/catalogue.log"   #defining log file path
 
-R="\e[31m"   
-G="\e[32m"   
-Y="\e[33m"    
-N="\e[0m"    
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.daws88s.online
 
-SCRIPT_DIR=$(pwd)
-MONGODB_HOST='mongodb.devopspractice08.online'
-
-if [ $USERID -ne 0 ] ; then 
-    echo "$R Please run this script with root acess $N" | tee -a $LOGS_FILE     #run as root user 
-    exit 1 # exit scode
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
 fi
-    mkdir -p $LOGS_FOLDER  #creating logs folder if not exists
 
-# Function to validate the installation status
+mkdir -p $LOGS_FOLDER
+
 VALIDATE(){
-     
-    if [ $1 -ne 0 ] ; then
-        echo " $2 $Rinstallation failed...$N" | tee -a $LOGS_FILE 
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
-
-        echo " $2 $Ginstallation successful....$N"  | tee -a $LOGS_FILE
-fi
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
 }
 
-dnf module disable nodejs -y &>> $LOGS_FILE
-VALIDATE $? "Disabling Nodejs module"   
+dnf module disable nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Disabling NodeJS Default version"
 
-dnf module enable nodejs:20 -y &>> $LOGS_FILE
-VALIDATE $? "Enabling Nodejs 20 module"
+dnf module enable nodejs:20 -y &>>$LOGS_FILE
+VALIDATE $? "Enabling NodeJS 20"
 
-dnf install nodejs -y &>> $LOGS_FILE
-VALIDATE $? "Nodejs installation"
+dnf install nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Install NodeJS"
 
-id roboshop &>> $LOGS_FILE
-if [ $? -ne 0  ]; then
-
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>> $LOGS_FILE
-VALIDATE $? "Adding roboshop user"
+id roboshop &>>$LOGS_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+    VALIDATE $? "Creating system user"
 else
-echo -e " roboshop user already exits...$Y SKIPPING $N" &>> $LOGS_FILE
-fi  
-
-mkdir -p /app
-VALIDATE $? "Creating application directory"
-
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>> $LOGS_FILE
-VALIDATE $? "Downloading catalogue application content"
-
-cd /app 
-VALIDATE $? "Changing directory to /app"
-rm -rf /app/*
-VALIDATE $? "removing existing code"
-
-
-unzip -o /tmp/catalogue.zip &>> $LOGS_FILE
-VALIDATE $? "unzip catalogue  content"
-
-
-npm install &>> $LOGS_FILE 
-VALIDATE $? "Installing application dependencies"
-
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
-VALIDATE $? "created systemctl service file"
-
-systemctl daemon-reload
-VALIDATE $? "Reloading systemctl daemon"
-
-systemctl enable catalogue  &>> $LOGS_FILE
-VALIDATE $? "Enabling catalogue service"
-
-systemctl start catalogue
-VALIDATE $? "Starting catalogue service"
-
-
-
-cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-mongosh -y &>> $LOGS_FILE
-VALIDATE $? "Installing Mongodb client"
-
-if [ "$INDEX" -lt 0 ]; then
-  mongosh --host $MONGODB_HOST </app/db/master-data.js &>> $LOGS_FILE
-VALIDATE $? "Loading catalogue schema"
-else
-  echo -e " catalogue schema already exists...$Y SKIPPING $N" | tee -a $LOGS_FILE
+    echo -e "Roboshop user already exist ... $Y SKIPPING $N"
 fi
 
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
 
-systemctl restart catalogue 
-VALIDATE $? "Restarting catalogue service"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>>$LOGS_FILE
+VALIDATE $? "Downloading catalogue code"
+
+cd /app
+VALIDATE $? "Moving to app directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
+unzip /tmp/catalogue.zip &>>$LOGS_FILE
+VALIDATE $? "Uzip catalogue code"
+
+npm install  &>>$LOGS_FILE
+VALIDATE $? "Installing dependencies"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Created systemctl service"
+
+systemctl daemon-reload
+systemctl enable catalogue  &>>$LOGS_FILE
+systemctl start catalogue
+VALIDATE $? "Starting and enabling catalogue"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
+
+INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js
+    VALIDATE $? "Loading products"
+else
+    echo -e "Products already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl restart catalogue
+VALIDATE $? "Restarting catalogue"
--- mongodb.sh
+++ mongodb.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
- USERID=$(id -u)
-LOGS_FOLDER="/var/logs/shell-roboshop"  #defining logs folder path
-LOGS_FILE="$LOGS_FOLDER/$0.log"   #defining log file path
 
-R="\e[31m"   # Red color
-G="\e[32m"   # Green color
-Y="\e[33m"   # Yellow color 
-N="\e[0m"    # No Color
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-if [ $USERID -ne 0 ] ; then 
-    echo "$R Please run this script with root acess $N" | tee -a $LOGS_FILE     #run as root user 
-    exit 1 # exit scode
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
 fi
-    mkdir -p $LOGS_FOLDER  #creating logs folder if not exists
 
-# Function to validate the installation status
+mkdir -p $LOGS_FOLDER
+
 VALIDATE(){
-     
-    if [ $1 -ne 0 ] ; then
-        echo " $2 $Rinstallation failed...$N" | tee -a $LOGS_FILE # error message 
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
-
-        echo " $2 $Ginstallation successful....$N"  | tee -a $LOGS_FILE
-fi
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
 }
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying Mongo Repo" 
+
+dnf install mongodb-org -y &>>$LOGS_FILE
+VALIDATE $? "Installing MongoDB server"
+
+systemctl enable mongod &>>$LOGS_FILE
+VALIDATE $? "Enable MongoDB"
+
+systemctl start mongod
+VALIDATE $? "Start MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections"
+
+systemctl restart mongod
+VALIDATE $? "Restarted MongoDB"
