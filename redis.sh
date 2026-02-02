cbaadf10eb74c13061db19c9ce7bdeaef3c50a36
Vineethk08 <milatariv@mail.sacredheart.edu>
2026-02-02 14:56:04 -0500

    learning

--- redis.sh
+++ redis.sh
@@ -24,20 +24,20 @@ VALIDATE(){
     fi
 }
 
-dnf module disable redis -y
+dnf module disable redis -y &>>$LOGS_FILE
 VALIDATE $? "Disabling Redis Default version"
 
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y    &>>$LOGS_FILE
 VALIDATE $? "Enabling Redis 7"
 
 dnf install redis -y &>>$LOGS_FILE
 VALIDATE $? "Installing Redis"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
-VALIDATE $? "Updating Redis Configuration file"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/Protected-mode/Protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Updating Redis Configuration"
 
-systemctl enable redis 
+systemctl enable redis  &>>$LOGS_FILE
 VALIDATE $? "Enabling Redis Service"
 
-systemctl start redis 
+systemctl start redis  &>>$LOGS_FILE
 VALIDATE $? "Starting Redis Service"
